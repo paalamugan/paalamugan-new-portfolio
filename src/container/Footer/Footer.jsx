55f1591c7fcc5e19b599e72849e25be85cb9b7db
paalamugan <paalamugan@gmail.com>
2022-05-06 17:03:08 +0530

    Fix: Footer alignment issue

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.scss';
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <footer className="flex items-center justify-center px-4 py-4 mb-4 bg-white">
+    <footer className="flex items-center justify-center px-4 pb-8 bg-white">
       <p className="text-base text-center text-black">© {year} Paalamugan. All Rights Reserved.</p>
     </footer>
   );
